33e5b79e21c1096be753cd4cc57e35bdde87e7b8
William DURAND <william.durand1@gmail.com>
2013-01-14 01:11:20 +0100

    Fix syntax HL

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-OPTIONS="--highlight-style=zenburn  --standalone -t html -f markdown"
+OPTIONS="--highlight-style=tango --standalone -t html -f markdown"
 `pandoc $OPTIONS -o html/tp1/0-introduction.html src/0-introduction.markdown`
 `pandoc $OPTIONS -o html/tp1/1-discover-php.html src/1-discover-php.markdown`
 `pandoc $OPTIONS -o html/tp2/2-implement-PSR-0.html src/2-implement-PSR-0.markdown`
